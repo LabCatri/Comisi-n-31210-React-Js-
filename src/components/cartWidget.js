59f209219ca41e6da235cb9c1379ad47d0ab9e14
LabCatri <labcatri@gmail.com>
2022-06-23 23:55:52 -0300

    solve

---
+++ src/components/cartWidget.js
@@ -0,0 +1,9 @@
+
+import shopping_cart from "../assets/img/cart.png"
+const cartWidget () {
+    return (
+        <div><img src="shopping_cart" alt="Cart Icon" /></div>
+    );
+  }
+
+  export default cartWidget
